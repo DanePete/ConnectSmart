6ef89fc8dbcb971542a55aebdc6676b030170cd0
Dane Petersen <danepetersen@Danes-MBP.home>
2021-03-30 12:37:09 -0500

    imported material ui button into app.js and displayed a Hello World Btn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
