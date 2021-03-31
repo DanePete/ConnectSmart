9dbc68b9909814035d20541778fb87cfadb6252c
Dane Petersen <danepetersen@Danes-MBP.home>
2021-03-30 22:00:01 -0500

    imrpovements to authentication checks

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
-import {AmplifySignOut, AmplifyAuthenticator, AmplifySignUp} from '@aws-amplify/ui-react';
+import {AmplifySignOut, AmplifyAuthenticator} from '@aws-amplify/ui-react';
 import AddItem from "./components/add";
-
-// import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
 import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
 
 Amplify.configure(awsconfig)
@@ -23,64 +21,6 @@ function App() {
       setUser(authData);
     });
   }, []);
-
-  // return (
-  //   <AmplifyAuthenticator>
-  //     <AmplifySignUp
-  //       slot="sign-up"
-  //       usernameAlias="username"
-  //       formFields={[
-  //         {
-  //           type: "email",
-  //           label: "Enter your email",
-  //           placeholder: "example@example.com",
-  //           required: true,
-  //         },
-  //         {
-  //           type: "password",
-  //           label: "Password",
-  //           placeholder: "don't use a dumb password",
-  //           required: true,
-  //         },
-  //         {
-  //           type: "preferred_username",
-  //           label: "Username",
-  //           placeholder: "Enter something cool, don't want to look like a noob",
-  //           required: true,
-  //         },
-  //       ]} 
-  //     />
-  //     <div className="App">
-
-  //     <AlwaysOn/>
-  //       <Button variant="contained" color="primary">
-  //         <AmplifySignOut />
-  //       </Button>
-
-  //       {/* 
-  //         * Display the Add Item component we created. 
-  //       */}
-  //       <AddItem />
-        
-  //       <header className="App-header">
-  //         <img src={logo} className="App-logo" alt="logo" />
-  //         <AmplifySignOut />
-  //         <h2>My App Content</h2>
-  //         <p>
-  //           Edit <code>src/App.js</code> and save to reload.
-  //         </p>
-  //         <a
-  //           className="App-link"
-  //           href="https://reactjs.org"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //         >
-  //           Learn React
-  //         </a>
-  //       </header>
-  //     </div>
-  //   </AmplifyAuthenticator>
-  // );
 
   return authState === AuthState.SignedIn && user ? (
     <div className="App">
