9951a755ec6ffa5416e36d5a53ffdbed681bcadb
Dane Petersen <danepetersen@Danes-MBP.home>
2021-03-30 21:55:39 -0500

    installed react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 import Button from '@material-ui/core/Button';
 import Amplify from 'aws-amplify';
@@ -6,64 +7,96 @@ import awsconfig from './aws-exports';
 import {AmplifySignOut, AmplifyAuthenticator, AmplifySignUp} from '@aws-amplify/ui-react';
 import AddItem from "./components/add";
 
+// import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+
 Amplify.configure(awsconfig)
 
 function App() {
-  return (
-    <AmplifyAuthenticator>
-      <AmplifySignUp
-        slot="sign-up"
-        usernameAlias="username"
-        formFields={[
-          {
-            type: "email",
-            label: "Enter your email",
-            placeholder: "example@example.com",
-            required: true,
-          },
-          {
-            type: "password",
-            label: "Password",
-            placeholder: "don't use a dumb password",
-            required: true,
-          },
-          {
-            type: "preferred_username",
-            label: "Username",
-            placeholder: "Enter something cool, don't want to look like a noob",
-            required: true,
-          },
-        ]} 
-      />
-      <div className="App">
-        <Button variant="contained" color="primary">
-          <AmplifySignOut />
-        </Button>
 
-        {/* 
-          * Display the Add Item component we created. 
-        */}
-        <AddItem />
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  // return (
+  //   <AmplifyAuthenticator>
+  //     <AmplifySignUp
+  //       slot="sign-up"
+  //       usernameAlias="username"
+  //       formFields={[
+  //         {
+  //           type: "email",
+  //           label: "Enter your email",
+  //           placeholder: "example@example.com",
+  //           required: true,
+  //         },
+  //         {
+  //           type: "password",
+  //           label: "Password",
+  //           placeholder: "don't use a dumb password",
+  //           required: true,
+  //         },
+  //         {
+  //           type: "preferred_username",
+  //           label: "Username",
+  //           placeholder: "Enter something cool, don't want to look like a noob",
+  //           required: true,
+  //         },
+  //       ]} 
+  //     />
+  //     <div className="App">
+
+  //     <AlwaysOn/>
+  //       <Button variant="contained" color="primary">
+  //         <AmplifySignOut />
+  //       </Button>
+
+  //       {/* 
+  //         * Display the Add Item component we created. 
+  //       */}
+  //       <AddItem />
         
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <AmplifySignOut />
-          <h2>My App Content</h2>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <AmplifySignOut />
+  //         <h2>My App Content</h2>
+  //         <p>
+  //           Edit <code>src/App.js</code> and save to reload.
+  //         </p>
+  //         <a
+  //           className="App-link"
+  //           href="https://reactjs.org"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Learn React
+  //         </a>
+  //       </header>
+  //     </div>
+  //   </AmplifyAuthenticator>
+  // );
+
+  return authState === AuthState.SignedIn && user ? (
+    <div className="App">
+      <AddItem />
+      <AmplifySignOut />
+      {/* <ListItems /> */}
+    </div>
+  ) : (
+    <div className="container">
+      <div className="signIn">
+        <AmplifyAuthenticator />
       </div>
-    </AmplifyAuthenticator>
+    </div>
   );
+
+
 }
 
 export default App;
