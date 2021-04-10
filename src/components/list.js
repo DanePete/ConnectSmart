79d9860c66e8563db6022ec7f54a986664df805c
Dane Petersen <danepetersen@Danes-MBP.home>
2021-04-09 20:54:16 -0500

    installed redux into project for state

--- src/components/list.js
+++ src/components/list.js
@@ -2,34 +2,34 @@
 import React from "react";
 import { Button, Table} from 'react-bootstrap';
 import 'react-toastify/dist/ReactToastify.css';
-import Moment from 'react-moment';
 
 const List = ({data}) => {
-    return (
-        <div>
-        <Table striped bordered hover variant="dark" responsive>
-          <thead>
-            <th>Title</th>
-            <th>Owner</th>
-            <th colSpan="2"></th>
-          </thead>
-          <tbody>
-          {data.sort((a, b) => a.title > b.title ? 1 : -1).map(data => {
-            return (
-              <tr>
-                <td>{data.title}</td>
-                <td>{data.owner}</td>
-                <td><Button variant="info">Edit</Button></td>
-                <td><Moment format="MM/DD/YYYY, hh:mm A">{data.updatedAt}</Moment></td>
-                {/* <td><Button onClick={() => deleteItem(data.id)}>Delete!</Button></td> */}
-              </tr>
-            )
-          })}
-          </tbody>
-        </Table>
-      </div>
-    );
+    // return (
+    //     <div>
+    //     <Table striped bordered hover variant="dark" responsive>
+    //       <thead>
+    //         <th>Title</th>
+    //         <th>Owner</th>
+    //         <th colSpan="2"></th>
+    //       </thead>
+    //       <tbody>
+    //       {data.map(data => {
+    //         return (
+    //           <tr>
+    //             <td>{data.title}</td>
+    //             <td>{data.owner}</td>
+    //             <td><Button variant="info">Edit</Button></td>
+    //             <td>{data.id}</td>
+    //             {/* <td><Button onClick={() => deleteItem(data.id)}>Delete!</Button></td> */}
+    //           </tr>
+    //         )
+    //       })}
+    //       </tbody>
+    //     </Table>
+    //   </div>
+    // );
 };
+  
   export default List;
   
   
