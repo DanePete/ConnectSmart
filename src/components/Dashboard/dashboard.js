ffb0666dbb71a9a93bf656fa044f9eaee61e21a2
Dane Petersen <dane.m.petersen@gmail.com>
2021-04-11 11:42:29 -0500

    added hi mom to show my mother how git works

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -6,6 +6,9 @@ import 'react-toastify/dist/ReactToastify.css';
   return (
     <div>
         <h1>Dashboard</h1>
+
+        <h1>HI MOM</h1>
+
         <h5>This page is a work in progress</h5>
     </div>
   );
