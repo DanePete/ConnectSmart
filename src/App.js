9ccfcecc263db4a4868719827adda421bbbf5eab
Dane Petersen <dane.m.petersen@gmail.com>
2021-04-11 19:20:03 -0500

    added loading indicator to homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
@@ -17,17 +17,33 @@ import Dashboard from "./components/Dashboard/dashboard";
 import EditSoftware from "./components/Software/editSoftware";
 import Header from "./components/Header/header";
 import Company from "./components/Company/company";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import { css } from "@emotion/core";
 
 
 
 Amplify.configure(awsconfig)
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [authState, setAuthState] = React.useState();
   const [user, setUser] = React.useState();
   const store = createStore(() => [], {}, applyMiddleware());
 
+  const override = css`
+    display: block;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    /* bring your own prefixes */
+    transform: translate(-50%, -50%);
+  `;
+
   React.useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false)
+    }, 4000)
     return onAuthUIStateChange((nextAuthState, authData) => {
       setAuthState(nextAuthState);
       setUser(authData);
@@ -38,32 +54,35 @@ function App() {
     <Provider store={store}>
       <Router>
         <listener />
-        <div className="App">
-          <ToastContainer />
-          <SideBar />
-          <div className="main">
-            <Header />
-            {/* <AmplifyS3Image imgKey="Sketch.png" />
-            <AmplifyS3ImagePicker /> */}
-            <Switch>
-              <Route exact path="/">
-                <Dashboard />
-              </Route>
-              <Route path="/software">
-                <Software />
-              </Route>
-              <Route exact path="/users">
-                <Users />
-              </Route>
-              <Route exact path="/companies">
-                <Company />
-              </Route>
-              <Route exact path="/editSoftware/:id/:name" component={EditSoftware}>
-              
-              </Route>
-            </Switch> 
+
+          <div className="App">
+            <ToastContainer />
+            <SideBar />
+            { loading ? <ClimbingBoxLoader css={override} size={50} color={"#ff9900"} loading={loading} /> : 
+              <div className="main">
+                <Header />
+                {/* <AmplifyS3Image imgKey="Sketch.png" />
+                <AmplifyS3ImagePicker /> */}
+                <Switch>
+                  <Route exact path="/">
+                    <Dashboard />
+                  </Route>
+                  <Route path="/software">
+                    <Software />
+                  </Route>
+                  <Route exact path="/users">
+                    <Users />
+                  </Route>
+                  <Route exact path="/companies">
+                    <Company />
+                  </Route>
+                  <Route exact path="/editSoftware/:id/:name" component={EditSoftware}>
+                  
+                  </Route>
+                </Switch> 
+              </div>
+            }
           </div>
-        </div>
       </Router>
     </Provider>
   )  : (
